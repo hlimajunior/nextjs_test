3b2d8b1a6b4e0ac9b08894a173f9312475067890
Humberto Lima Júnior <hlimajunior@gmail.com>
2023-07-11 14:22:42 -0300

    revalidate

--- pages/tempo.js
+++ pages/tempo.js
@@ -4,20 +4,24 @@ function Tempo(props) {
     return (
         <div>
             <div>{dynamicDateString} (dinâmico)</div>
-            <div>{props.staticDateString} (estático)</div>
+            <div>{props.staticDateString} (estático - com delay)</div>
         </div>
     )
 }
 
-export function getStaticProps() {
+export async function getStaticProps() {
+    console.log('> Passando pelo getStaticProps');
     const staticDate = new Date();
     const staticDateString = staticDate.toGMTString();
 
     return {
         props: {
             staticDateString
-        }
+        },
+        revalidate: 10
     }
 }
+
+const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
 
 export default Tempo;
